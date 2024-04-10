9b669e3d5f7dea2970b9ac017f0e22a00ddabd4c
aekkakti <music.tomsk1@gmail.com>
2024-04-10 13:41:30 +0700

    Почти доделана стилизация авторизации

--- views/site/login.php
+++ views/site/login.php
@@ -1,14 +1,16 @@
 <?php
 if (!app()->auth::check()):
     ?>
-<div class="frame">
-    <div class="login_form">
-        <h2>Авторизация</h2>
-        <form method="post">
-            <label>Имя пользователя <input type="text" name="nickname"></label><br><br>
-            <label>Пароль <input type="password" name="password"></label><br><br>
-            <button>Авторизация</button>
-        </form>
+<div class="container">
+    <div class="frame b-w">
+        <div class="login_form b-w">
+            <h2 class="authText b-w">Авторизация</h2><br><br>
+            <form method="post" class="b-w inputLoginForm">
+                <label>Логин <input type="text" name="nickname"></label><br><br>
+                <label>Пароль <input type="password" name="password"></label><br><br>
+                <button>Авторизация</button>
+            </form>
+        </div>
     </div>
 </div>
 <?php endif;
